ecacc322704a0eb17a493933fce05db84442776f
Xavier Buckner <xavier@Xaviers-MacBook-Pro.local>
2023-01-20 16:57:40 -0600

    student testing added

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -1,5 +1,9 @@
 // Import the Spinner component into this file and test
+import Spinner from './Spinner'
+import React from 'react'
+import { render, screen, wait, fireEvent} from "@testing-library/react"
 // that it renders what it should for the different props it can take.
+
 test('sanity', () => {
-  expect(true).toBe(false)
+  render(<Spinner/>)
 })
